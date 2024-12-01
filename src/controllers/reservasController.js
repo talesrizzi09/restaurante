145c8f97d7400a3790b2aea67bcbdc97e37bcf65
talesrizzi09 <128074493+talesrizzi09@users.noreply.github.com>
2024-12-01 20:57:53 -0300

    Update reservasController.js

--- src/controllers/reservasController.js
+++ src/controllers/reservasController.js
@@ -1,7 +1,7 @@
 const Reserva = require('../model/reserva'); 
 const twilio = require ('twilio');
 const accountSid = 'ACdc931f028e2a1b0d05a36e1eca67b2fb'; // Substitua com suas credenciais
-const authToken = '3abac777aa984cc88d024825f6dce992'; // Substitua com suas credenciais
+const authToken = 'efa7b22da940632eba1b277c92b91547'; // Substitua com suas credenciais
 const client = twilio(accountSid, authToken);
 
 
